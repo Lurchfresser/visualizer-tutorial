f485d5f8578c3957d2d9ecec6801ed729bab9045
lurchfresser <Tritrop007@gmx.de>
2024-11-14 23:48:09 +0100

    Added tracks

--- script.js
+++ script.js
@@ -16,19 +16,25 @@ let audioSource;
  */
 let analyser;
 
+
+/**
+     * @type {HTMLAudioElement}
+     */
+const audioElement = document.querySelector('#audio');
+audioElement.src = 'overthinker-full.mp3';
+//let audio = new Audio('overthinker.mp3');
+const audioCtx = new AudioContext();
+
+audioElement.play();
+audioSource = audioCtx.createMediaElementSource(audioElement);
+analyser = audioCtx.createAnalyser();
+audioSource.connect(analyser);
+analyser.connect(audioCtx.destination);
+analyser.fftSize = 64;
+
+
 container.addEventListener('click', () => {
-
-    const audio = document.querySelector('#audio');
-    audio.src = 'overthinker.mp3';
-    //let audio = new Audio('overthinker.mp3');
-    const audioCtx = new AudioContext();
-
-    audio.play();
-    audioSource = audioCtx.createMediaElementSource(audio);
-    analyser = audioCtx.createAnalyser();
-    audioSource.connect(analyser);
-    analyser.connect(audioCtx.destination);
-    analyser.fftSize = 64;
+    
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
 
@@ -41,7 +47,10 @@ container.addEventListener('click', () => {
         analyser.getByteFrequencyData(dataArray);
         for (let i = 0; i < bufferLength; i++) {
             barHeight = dataArray[i];
-            ctx.fillStyle = 'white';
+            const r = barHeight + (25 * (i / bufferLength));
+            const g = 250 * (i / bufferLength);
+            const b = 50;
+            ctx.fillStyle = `rgb(${r},${g},${b})`;
             ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
             x += barWidth;
         }
