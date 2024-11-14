e6337db99c49576b068d0fdba8b7e00f1f66b911
lurchfresser <Tritrop007@gmx.de>
2024-11-15 00:50:30 +0100

    animation now only playing when audio is playing

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ let fakeAudioSource = fakeAudioCtx.createMediaElementSource(fakeAudioElement);
  * @type {AnalyserNode}
  */
 let fakeAnalyser = fakeAudioCtx.createAnalyser();
-
+//TODO: Try different values
+fakeAnalyser.fftSize = 64;
 
 fakeAudioElement.destination = fakeAudioCtx.destination;
 
@@ -61,27 +62,39 @@ analyser.connect(audioCtxRealSong.destination);
 analyser.fftSize = 64;
 
 
-container.addEventListener('click', () => {
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    const barWidth = canvas.width / bufferLength;
-    let barHeight;
-
-    function animate() {
-        let x = 0;
-        drawCtx.clearRect(0, 0, canvas.width, canvas.height);
-        fakeAnalyser.getByteFrequencyData(dataArray);
-        for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            const r = barHeight + (25 * (i / bufferLength));
-            const g = 250 * (i / bufferLength);
-            const b = 50;
-            drawCtx.fillStyle = `rgb(${r},${g},${b})`;
-            drawCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-            x += barWidth;
-        }
-        requestAnimationFrame(animate);
-    }
+audioElement.addEventListener('play', () => {
+    fakeAudioElement.play();
     animate();
 });
+audioElement.addEventListener('pause', () => {
+    fakeAudioElement.pause();
+});
+//! is useful if u want to switch the time for debugging
+// audioElement.addEventListener('timeupdate', () => {
+//     fakeAudioElement.currentTime = audioElement.currentTime;
+// });
+
+const bufferLength = fakeAnalyser.frequencyBinCount;
+
+function animate() {
+    const dataArray = new Uint8Array(bufferLength);
+    const barWidth = canvas.width / bufferLength;
+    drawCtx.clearRect(0, 0, canvas.width, canvas.height);
+    if (audioElement.paused) {
+        return;
+    }
+    fakeAnalyser.getByteFrequencyData(dataArray);
+    let x = 0;
+    for (let i = 0; i < bufferLength; i++) {
+        let barHeight;
+        barHeight = dataArray[i];
+        const r = barHeight + (25 * (i / bufferLength));
+        const g = 250 * (i / bufferLength);
+        const b = 50;
+        drawCtx.fillStyle = `rgb(${r},${g},${b})`;
+        drawCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+    requestAnimationFrame(animate);
+
+}
